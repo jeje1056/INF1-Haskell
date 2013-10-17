9d4990e1512706553b41ee906a5ab109bb80d9a6
Aaron Cronin <cronin@include.cat>
2013-10-17 11:26:55 +0100

    Complete

--- Tutorial3/tutorial3.hs
+++ Tutorial3/tutorial3.hs
@@ -4,6 +4,7 @@
 -- Week 5 - Due: 17/18 Oct.
 
 import Data.Char
+import Data.List
 import Test.QuickCheck
 import Control.Monad
 
@@ -126,11 +127,11 @@ prop_concat strs = concatRec strs == concatFold strs
 
 -- d.
 rmCharsRec :: String -> String -> String
-rmCharsRec [] s = s
+rmCharsRec [] s          = s
 rmCharsRec (c:cs) string = rmCharsRec cs $ rmChar c string
 
 rmCharsFold :: String -> String -> String
-rmCharsFold = 
+rmCharsFold = flip $ foldr rmChar
 
 prop_rmChars :: String -> String -> Bool
 prop_rmChars chars str = rmCharsRec chars str == rmCharsFold chars str
@@ -143,21 +144,36 @@ type Matrix = [[Int]]
 -- 5
 -- a.
 uniform :: [Int] -> Bool
-uniform = undefined
+uniform l = all (== head l) l
 
 -- b.
 valid :: Matrix -> Bool
-valid = undefined
+valid matrix = rule_1 && rule_2
+  where
+    rule_1 = not (null matrix) && not (null $ head matrix)
+    rule_2 = uniform $ map length matrix
 
 -- 6.
+zipWith' f xs ys = [ f x y | (x, y) <- zip xs ys ]
+
+zipWith'' f xs ys = map (uncurry f) $ zip xs ys
 
 -- 7.
 plusM :: Matrix -> Matrix -> Matrix
-plusM = undefined
+plusM m1 m2
+  | not (valid m1 && valid m2)              = error "Matrices must be valid"
+  | length m1 /= length m2                  = error "Matrices must have the same number of Rows"
+  | (length $ head m1) /= (length $ head m2) = error "Matrices must have the same number of Columns"
+  | otherwise                               = (zipWith $ zipWith (+)) m1 m2
 
 -- 8.
 timesM :: Matrix -> Matrix -> Matrix
-timesM = undefined
+timesM m1 m2
+  | not (valid m1 && valid m2)   = error "Matrices must be valid"
+  | length (head m1) /= length m2 = error "Must be kxn and nxl Matrices"
+  | otherwise                    = map m3Row m1
+    where
+      m3Row m1_row = map (sum . zipWith (*) m1_row) $ transpose m2
 
 -- Optional material
 -- 9.
