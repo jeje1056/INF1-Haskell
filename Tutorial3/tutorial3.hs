58d6ebef34fe702981d1b0493e75bc849d0425e6
Aaron Cronin <cronin@include.cat>
2013-10-17 09:19:24 +0100

    First half

--- Tutorial3/tutorial3.hs
+++ Tutorial3/tutorial3.hs
@@ -1,58 +1,59 @@
--- Informatics 1 - Functional Programming 
+-- Informatics 1 - Functional Programming
 -- Tutorial 3
 --
 -- Week 5 - Due: 17/18 Oct.
 
 import Data.Char
 import Test.QuickCheck
+import Control.Monad
 
 
 
 -- 1. Map
 -- a.
 uppers :: String -> String
-uppers = undefined
+uppers = map toUpper
 
 -- b.
 doubles :: [Int] -> [Int]
-doubles = undefined
+doubles = map (* 2)
 
--- c.        
+-- c.
 penceToPounds :: [Int] -> [Float]
-penceToPounds = undefined
+penceToPounds = map ((/ 100) . fromIntegral)
 
 -- d.
 uppers' :: String -> String
-uppers' = undefined
+uppers' str = [ toUpper c | c <- str ]
 
 prop_uppers :: String -> Bool
-prop_uppers = undefined
+prop_uppers = liftM2 (==) uppers uppers'
 
 
 
 -- 2. Filter
 -- a.
 alphas :: String -> String
-alphas = undefined
+alphas = filter isAlpha
 
 -- b.
 rmChar ::  Char -> String -> String
-rmChar = undefined
+rmChar = filter . (/=)
 
 -- c.
 above :: Int -> [Int] -> [Int]
-above = undefined
+above = filter . (<)
 
 -- d.
 unequals :: [(Int,Int)] -> [(Int,Int)]
-unequals = undefined
+unequals = filter $ uncurry (/=)
 
 -- e.
 rmCharComp :: Char -> String -> String
-rmCharComp = undefined
+rmCharComp drop_char str = [ c | c <- str, c /= drop_char ]
 
 prop_rmChar :: Char -> String -> Bool
-prop_rmChar = undefined
+prop_rmChar c str = rmChar c str == rmCharComp c str
 
 
 
@@ -62,7 +63,7 @@ upperChars :: String -> String
 upperChars s = [toUpper c | c <- s, isAlpha c]
 
 upperChars' :: String -> String
-upperChars' = undefined
+upperChars' = map toUpper . filter isAlpha
 
 prop_upperChars :: String -> Bool
 prop_upperChars s = upperChars s == upperChars' s
@@ -72,7 +73,7 @@ largeDoubles :: [Int] -> [Int]
 largeDoubles xs = [2 * x | x <- xs, x > 3]
 
 largeDoubles' :: [Int] -> [Int]
-largeDoubles' = undefined
+largeDoubles' = map (* 2) . filter (> 3)
 
 prop_largeDoubles :: [Int] -> Bool
 prop_largeDoubles xs = largeDoubles xs == largeDoubles' xs 
@@ -82,7 +83,7 @@ reverseEven :: [String] -> [String]
 reverseEven strs = [reverse s | s <- strs, even (length s)]
 
 reverseEven' :: [String] -> [String]
-reverseEven' = undefined
+reverseEven' = map reverse . filter (even . length)
 
 prop_reverseEven :: [String] -> Bool
 prop_reverseEven strs = reverseEven strs == reverseEven' strs
@@ -96,37 +97,40 @@ productRec []     = 1
 productRec (x:xs) = x * productRec xs
 
 productFold :: [Int] -> Int
-productFold = undefined
+productFold = foldr (*) 1
 
 prop_product :: [Int] -> Bool
 prop_product xs = productRec xs == productFold xs
 
 -- b.
 andRec :: [Bool] -> Bool
-andRec = undefined
+andRec []     = True
+andRec (b:bs) = b && andRec bs
 
 andFold :: [Bool] -> Bool
-andFold = undefined
+andFold = foldr (&&) True
 
 prop_and :: [Bool] -> Bool
 prop_and xs = andRec xs == andFold xs 
 
 -- c.
 concatRec :: [[a]] -> [a]
-concatRec = undefined
+concatRec []       = []
+concatRec (l : ls) = l ++ concatRec ls
 
 concatFold :: [[a]] -> [a]
-concatFold = undefined
+concatFold = foldr (++) []
 
 prop_concat :: [String] -> Bool
 prop_concat strs = concatRec strs == concatFold strs
 
 -- d.
 rmCharsRec :: String -> String -> String
-rmCharsRec = undefined
+rmCharsRec [] s = s
+rmCharsRec (c:cs) string = rmCharsRec cs $ rmChar c string
 
 rmCharsFold :: String -> String -> String
-rmCharsFold = undefined
+rmCharsFold = 
 
 prop_rmChars :: String -> String -> Bool
 prop_rmChars chars str = rmCharsRec chars str == rmCharsFold chars str
